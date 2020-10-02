ab195e39ff5fb466cb2ac29cdd6b9c67065d55bc
KornelijaK <kornelijako@hotmail.com>
2020-10-02 15:39:58 +0200

    alert

--- jscodex.js
+++ jscodex.js
@@ -4,42 +4,43 @@
 
 //CONTROLING USERACCESS FORMS:
 
-//the inherit start of the doc:	
+//the inherit start of the doc:
 currentstate = "signin";
 function toggle_useraccess() {
-	
+
 	if (currentstate == "signin") { //if signin is displayed and button is pressed:
-		
+
 		document.getElementById("signin-form").style.display = "none";
 		document.getElementById("signup-form").style.display = "block";
-		
+
 		currentstate = "signup";
-		
+
 	} else {
-		
+
 		document.getElementById("signup-form").style.display = "none";
 		document.getElementById("signin-form").style.display = "block";
-		
+
 		currentstate = "signin";
-		
+
 	}
-	
+
 }
 
 
 
 function showinfo(el_id) {
-	
+
 	if (getComputedStyle(document.getElementById(el_id)).display == "none") {
-		
+
 		document.getElementById(el_id).style.display = "grid";
-		
+
 	} else {
-		
+
 		document.getElementById(el_id).style.display = "none";
-		
+		alert("hello");
+
 	}
-	
+
 	//check if style == grid: remove style
-	
+
 }
